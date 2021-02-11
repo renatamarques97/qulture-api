db20b5c944fd71bb090d8c98a0f0a245ccbfbdff
Renata Marques <renatamarques@Renatas-MacBook-Air.local>
2021-02-10 23:17:39 -0300

    add: error handling for not found and unprocessable entity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  skip_forgery_protection
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActiveRecord::RecordInvalid, with: :render_401
+
+  def render_404(error)
+    render json: { message: error.message }, status: :not_found
+  end
+
+  def render_401(error)
+    render json: { message: error.message }, status: :unprocessable_entity
+  end
 end
